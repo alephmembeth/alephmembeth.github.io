28509049af1738d4e7f67159e5f9363b3c6bf52d
Max <53583040+alephmembeth@users.noreply.github.com>
2026-02-21 12:25:19 +0100

    Update scale.fix.js

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -8,19 +8,18 @@
             }
         },
         initialize = function() {
-            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+            changeViewportContent("width = device-width, minimum-scale = 1.0, maximum-scale = 1.0");
         },
         gestureStart = function() {
-            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+            changeViewportContent("width = device-width, minimum-scale = 0.25, maximum-scale = 1.6");
         },
         gestureEnd = function() {
             initialize();
         };
-
-
+    
     if (navigator.userAgent.match(/iPhone/i)) {
         initialize();
-
+        
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
